1d13908dc5796748fd76fbd34eaad011864da5e7
tamilelavenil <tamilelavenil@gmail.com>
2023-08-06 16:43:33 +0530

    Updating changes

---
+++ java/classRoomWeekdays/SwapTwoNumWithoutThirdVariable.java
@@ -0,0 +1,19 @@
+package classRoomWeekdays;
+
+public class SwapTwoNumWithoutThirdVariable {
+
+	public static void main(String[] args) {
+		int x =60, y=100;
+		
+	     x = x+y;
+	     
+	     y = x - y;
+	     
+	     x = x -y;
+		
+		
+		System.out.println(x+" "+y);
+
+	}
+
+}
